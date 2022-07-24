f176870f9c0b23dc8b85fc8691fd9f9c7b77d19b
Lorena Morales <Lorenarm.999@gmail.com>
2022-07-24 13:08:07 -0600

    updated script and html to start creating elements outputing info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,117 @@
-//// if i input a city
 
-///random notes for now
+const searchBtnEl = document.querySelector(".searchBtn");
+const cityInputEl = document.querySelector(".searchBox");
+const searchForm = document.querySelector("#search-form");
+const section = document.querySelector(".appendsection")
 
-//converting a city to long and lat
+let city = localStorage.getItem("city")
 
-//2 seprate api calls 
 
-//geo coding //city name 
+const apiKeyVar = "cc74a924b91e51b9e01d7af51fb380f6";
 
-//anything in the world
 
-//fire off one api call
+searchBtnEl.addEventListener("click", () => {
+    //event.preventDefault();
+    searchCity (); 
+    saveLocalStorage ();
+});
 
-//separate calls/current weather 
+function saveLocalStorage () {
 
-//global variables  
-//nesting calls 
+    let userCity = cityInputEl.value.trim();
+    
+    let cityArray = [];
+    if (!city) {
+        cityArray.push(userCity);
+        localStorage.setItem("city", JSON.stringify(cityArray));
+    } else {
+        cityArray = cityArray.concat(JSON.parse(city)||0);
+        cityArray.push(userCity);
+        localStorage.setItem("city", JSON.stringify(cityArray));
+    }
+    renderButton ();
+}
 
-//local storage//create an array //goes last!
+function searchCity () {
+    let userCity = cityInputEl.value.trim();
+    //console.log ("this is working!");
+    
+   
+    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${apiKeyVar}`;
+    fetch(requestUrl)
+        .then(function(response){
+            return response.json()
+        })
+        .then (function (data) {
+            console.log(data);
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            fiveDayWeather (lat, lon);
+
+        })
+
+}
+
+function fiveDayWeather (lat, lon) {
+    let requestUrl2 = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${apiKeyVar}`
+
+    //let requestUrl2 = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${apiKeyVar2}`
+
+    fetch(requestUrl2)
+        .then(function(response) {
+            return response.json() 
+        })
+        .then(function(data){
+            console.log(data);
+            let currentDay = data.current.dt
+            console.log(currentDay)
+            let temp = data.current.temp
+            console.log(temp)
+            let wind = data.current.wind_speed
+            console.log(wind)
+            let humidity = data.current.humidity
+            console.log(humidity)
+            let uvi = data.current.uvi
+            console.log(uvi)
+
+            renderInfoCurrent(currentDay, temp, wind, humidity, uvi);
+        })
+}
+
+function renderInfoCurrent(currentDay, temp, wind, humidity, uvi) {
+    let userCity = cityInputEl.value.trim();
+    var container = document.createElement("div")
+    container.classList.add("container");
+    container.classList.add("weather-section");
+    var cityHeader = document.createElement("h2")
+    cityHeader.classList.add("info");
+    var citybody = document.createElement("p")
+    citybody.classList.add("info")
+    console.log(cityHeader);
+    cityHeader.textContent = `${userCity}: ${currentDay}`
+    container.append(cityHeader)
+
+
+    section.append(container);
+
+}
+
+function renderButton () {
+    var getItem = JSON.parse(localStorage.getItem("city"));
+    
+
+    if (city) {
+        for (let index = 0; index < getItem.length; index++) {
+            const createBtnEl = document.createElement("button");
+            createBtnEl.classList.add("mb-1");
+            createBtnEl.textContent = getItem[index];
+            searchForm.append(createBtnEl);
+        } 
+    }else {
+        console.log("none")
+    }
+    
+}
+
+renderButton ();
+
