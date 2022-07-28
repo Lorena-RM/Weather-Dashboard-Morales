1175a1839ca830d0b82a1786dead7540b393e9c8
Lorena Morales <Lorenarm.999@gmail.com>
2022-07-28 01:53:02 -0600

    removed unnecessary inline comments/changed styling/added README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Weather Dashboard - Lorena Morales
+
+On week 6 of my Coding Bootcamp, we created a [`Weather Dashboard`](https://lorena-rm.github.io/Weather-Dashboard-Morales/) website in which we can search current weather information of a city as well as the next 5 days.
+
+I used 2 API calls for this homework along side with moment.js and bootstrap. Other factors played into this website as well such as google fonts.
+
+![WeatherDashboard GIF][gif]
+
+[gif]: ./assets/images/Screen%20Recording.gif "Screen Recording of website"
+
+
+# About this Website
+
+
+## Weather Information
+
+This Website allows you to gather weather information of a city once it is searched for. Created from scratch, as a user I am presented with a blank screen with a search form on the left hand side to search for a city. when I type a city and `click` search OR hit the `ENTER` key the website renders the...
+* `current` weather information and is a larger display up top and,
+* the next 5 day weather cards are displayed underneath the main section
+
+> 📝 FUTURE NOTE: When we fetch information from the first API, it returns an array of cities with that city name provided in search form but, `only` renders information of the first city from array returned... in the future I would like to add maybe an auto complete of what city/state specifically of what the user wants and rendering a more precise search!
+
+## Past Searches
+
+When a city is searched, not only is information being rendered on the screen, but past cities searched will now be saved to local storage and added into a list of buttons to which the user can go back on and click to be able to render weather information again without having to type it back out!
+
+## 404 ❌
+
+I added a 404.html to be used as a catch all in my promise function incase i get anything other then a 200 code, the screen will be rendered to a message saying 
+
+>     `"oops something went wrong, try typing a city like Denver etc.."`
+
+This same page will also pop up if nothing is typed into the search form and left in blank.  
+
+## Deployed Website 🌦
+
+A link was Provided at the top of this page..
+
+But this one also works! 😅 ----> [`Weather Dashboard`](https://lorena-rm.github.io/Weather-Dashboard-Morales/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,56 +5,55 @@ const section = document.querySelector(".appendsection");
 const btnAppend = document.querySelector(".buttonAppend");
 const hidden = document.querySelector(".hidden");
 
+//parse coming out of local storage
 let citiesAlreadySearched =
 JSON.parse(localStorage.getItem("citiesAlreadySearched")) || [];
 
-
+//api key ->in the future i will use a gitignore for this
 const apiKeyVar = "cc74a924b91e51b9e01d7af51fb380f6";
 
+//this event listener will grab the user input value and trigger the search city function with that value
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let userCity = cityInputEl.value.trim();
   searchCity(userCity);
 });
 
-function saveToLocalStorage(cityName) {
-  // if citiesAlreadySearched does not contain the cityName, then add city to the array and renderbuttons()
-
-  if (!citiesAlreadySearched.includes(cityName)) {
-    citiesAlreadySearched.push(cityName);
-    localStorage.setItem(
-      "citiesAlreadySearched",
-      JSON.stringify(citiesAlreadySearched)
-    );
-    renderButtons();
-  }
-}
-
+//searchcity function
 function searchCity(cityName) {
   
-
+  //first api reuqest is used to get city information to obtain a lat & lon
   let requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKeyVar}`;
+  //1st promise
   fetch(requestUrl)
     .then(function (response) {
-      return response.json();
+      //if the response is not 200
+      if (response.status !== 200){
+        //this replaces the html withe the 404 html
+        document.location.replace('./404.html')
+      } else {
+         return response.json();   
+      }
     })
     .then(function (data) {
-      
+      //grabbing data from response
+      //grabes first city form array of cities
       const cityResponse = data[0].name;
+      //calling a function to save into local storage
       saveToLocalStorage(cityResponse);
-      let lat = data[0].lat;
-      let lon = data[0].lon;
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      //calling function for next api call 
       fiveDayWeather(lat, lon, cityResponse);
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch(function () {
+      //this is in case no city is found.. a catch all/also replaces html to 404
+      document.location.replace('./404.html')
     });
 }
 
 function fiveDayWeather(lat, lon, cityName) {
   let requestUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${apiKeyVar}`;
-
-  //let requestUrl2 = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=${apiKeyVar2}`
 
   fetch(requestUrl2)
     .then(function (response) {
@@ -62,63 +61,54 @@ function fiveDayWeather(lat, lon, cityName) {
     })
     .then(function (data) {
       
-      let currentDay = data.current.dt*1000;
-      //let formatDate = moment(currentDay).format("MM/DD/YYYY");
-      
-      let icon = data.current.weather[0].icon;
-      
+      //declaring variables for current data
+      const icon = data.current.weather[0].icon;
+      const temp = data.current.temp;
+      const wind = data.current.wind_speed;
+      const humidity = data.current.humidity;
+      const uvi = data.current.uvi;
      
-      //console.log(currentDay)
-      let temp = data.current.temp;
-      //console.log(temp)
-      let wind = data.current.wind_speed;
-      //console.log(wind)
-      let humidity = data.current.humidity;
-      //console.log(humidity)
-      let uvi = data.current.uvi;
-      //console.log(uvi)
+      //declaring variables for future data
       let day1 = data.daily[0];
-      //console.log(day1);
       let day2 = data.daily[1];
-      //console.log(day2);
       let day3 = data.daily[2];
-      //console.log(day3);
       let day4 = data.daily[3];
-      //console.log(day4);
       let day5 = data.daily[4];
-      //console.log(day5);
 
+      //creating Array for daily, using declared variables
       let future5DayArray = [day1, day2, day3, day4, day5];
-      //console.log(future5DayArray);
-
+      
+      //calling to funtions to display current and the next 5 days
       renderInfoCurrent(cityName, icon, temp, wind, humidity, uvi);
-      renderFutureWeather(future5DayArray, );
+      renderFutureWeather(future5DayArray);
     });
 }
 
+//using the parameters passed from fivedayweather function i am able to use that data information in this function 
 function renderInfoCurrent(cityName, icon, temp, wind, humidity, uvi) {
+  //will clear out section each time a new city is searched/prevents duplicates
   section.innerHTML = "";
-  var formatDate = moment().format("MM/DD/YYYY");
-  var img = document.createElement("img");
+
+  //using moment to get current date 
+  let formatDate = moment().format("MM/DD/YYYY");
+  //creating a img EL and adding a src to it with parameters around icon so it can change depending on the icon code the weather renders 
+  const img = document.createElement("img");
   img.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
  
-  var container = document.createElement("div");
+  //creating a div to start creating my container for curent weather section
+  const container = document.createElement("div");
   container.classList.add("container", "weather-section");
-
-  var cityHeader = document.createElement("h2");
-  cityHeader.classList.add("info");
-  var cityTemp = document.createElement("p");
-  cityTemp.classList.add("info");
-  var cityWind = document.createElement("p");
-  cityWind.classList.add("info");
-  var cityHumidity = document.createElement("p");
-  cityHumidity.classList.add("info");
-  var cityUvi = document.createElement("p");
-  cityUvi.classList.add("info");
-  let uviCode = document.createElement("span");
-  var headerFuture = document.createElement("h2");
+  //elements that will go inside on container
+  const cityHeader = document.createElement("h2");
+  const cityTemp = document.createElement("p");
+  const cityWind = document.createElement("p");
+  const cityHumidity = document.createElement("p");
+  const cityUvi = document.createElement("p");
+  const uviCode = document.createElement("span");
+  //element that is created for title for future5dayforecast
+  const headerFuture = document.createElement("h2");
   headerFuture.classList.add("mb-3", "mt-3");
-
+  //text content for elements that were created
   cityHeader.textContent = `${cityName} (${formatDate})`;
   cityHeader.append(img)
   cityTemp.textContent = `Temp: ${temp} °F`;
@@ -126,72 +116,83 @@ function renderInfoCurrent(cityName, icon, temp, wind, humidity, uvi) {
   cityHumidity.textContent = `Humidity: ${humidity}%`;
   cityUvi.textContent = "UV Index: ";
   uviCode.textContent = `${uvi}`;
+
   headerFuture.textContent = "5-Day Forecast:";
-
+  //uvi seprate because span is being appended to uvi index
+  //sseparate because that number specifically is being color coded
   cityUvi.append(uviCode);
+  //appending all elements to container 
   container.append(cityHeader, cityTemp, cityWind, cityHumidity, cityUvi);
-
+  //appedning container and the header for future to the section
   section.append(container, headerFuture);
-
+  //finally calling another function for adding class codes
   classCodesForUvi (uvi, uviCode);
 }
 
 function  classCodesForUvi(uvi, uviCode) {
+  //conditional to help us determine which class its going to add specifically depending on the uvi number 
     if (uvi <= 2 ) {
         uviCode.classList.add("uviCodeLow");   
     } else if (uvi <= 5) {
         uviCode.classList.add("uviCodeModerate");   
-    } else if (uvi <= 7) {
+    } else if (uvi <= 8) {
         uviCode.classList.add("uviCodeHigh");   
     } else if (uvi > 8) {
         uviCode.classList.add("uviCodeVeryHigh");   
     }
 }
 
+
 function renderFutureWeather(future5DayArray) {
-  var secContainer = document.createElement("div");
+  //this is outside of the for loop because these are elements i do not want to repeat for each day of the 5day array
+  const secContainer = document.createElement("div");
   secContainer.classList.add("container");
-  var cardConatiner = document.createElement("div");
+  const cardConatiner = document.createElement("div");
   cardConatiner.classList.add("row", "row-col-5", "cardcon");
   secContainer.append(cardConatiner);
-  for (let index = 0; index < future5DayArray.length; index++) {
-    // const element = future5DayArray[index];
-    // console.log(element);
-    let futureUvi = future5DayArray[index].uvi
-    var formatDate = moment(future5DayArray[index].dt*1000).format("MM/DD/YYYY");
 
-    var img = document.createElement("img");
+  for (let index = 0; index < future5DayArray.length; index++) {
+    //creating an variable for futureuvi as it needs to go through a forloop to add determining classes
+    let futureUvi = future5DayArray[index].uvi
+    //using moment to obtain data for each object in array
+    let formatDate = moment(future5DayArray[index].dt*1000).format("MM/DD/YYYY");
+    //creating im element and adding img source to it
+    const img = document.createElement("img");
     let dailyIcons = future5DayArray[index].weather[0].icon;
     img.src = `https://openweathermap.org/img/wn/${dailyIcons}@2x.png`;
    
-    var col = document.createElement("div");
+    //creating elements that go inside each card (each day)
+    const col = document.createElement("div");
     col.classList.add("col");
-    var textBoxEl = document.createElement("div");
+    const textBoxEl = document.createElement("div");
     textBoxEl.classList.add("p-3", "media-p");
-    var textDay = document.createElement("h4");
-    var textTemp = document.createElement("p");
-    var textWind = document.createElement("p");
-    var textHumidity = document.createElement("p");
-    var textUvi = document.createElement("p");
-    let spanUvi = document.createElement("span");
+    const textDay = document.createElement("h4");
+    const textTemp = document.createElement("p");
+    const textWind = document.createElement("p");
+    const textHumidity = document.createElement("p");
+    const textUvi = document.createElement("p");
+    const spanUvi = document.createElement("span");
 
+    //uvi for loop
     if (futureUvi <= 2 ) {
         spanUvi.classList.add("uviCodeLow");   
     } else if (futureUvi <= 5) {
         spanUvi.classList.add("uviCodeModerate");   
-    } else if (futureUvi <= 7.99) {
+    } else if (futureUvi <= 8) {
         spanUvi.classList.add("uviCodeHigh");   
     } else if (futureUvi > 8) {
         spanUvi.classList.add("uviCodeVeryHigh");   
     }
 
+    //adding text content to elements created
     textDay.textContent = formatDate;
     textTemp.textContent = `Temp: ${future5DayArray[index].temp.max} °F`;
     textWind.textContent = `Wind: ${future5DayArray[index].wind_speed} MPH`;
     textHumidity.textContent = `Humidity: ${future5DayArray[index].humidity}%`;
     textUvi.textContent = "UV index: ";
     spanUvi.textContent = futureUvi;
-
+    
+    //appending corresponding elements
     textUvi.append(spanUvi);
     textBoxEl.append(textDay, img, textTemp, textWind, textHumidity, textUvi);
     col.append(textBoxEl);
@@ -200,15 +201,30 @@ function renderFutureWeather(future5DayArray) {
   section.append(secContainer);
 }
 
+function saveToLocalStorage(cityName) {
+  // if citiesAlreadySearched does not contain the cityName, then add city to the array and renderbuttons()
+  if (!citiesAlreadySearched.includes(cityName)) {
+    citiesAlreadySearched.push(cityName);
+    localStorage.setItem(
+      "citiesAlreadySearched",
+      JSON.stringify(citiesAlreadySearched)
+    );
+    //calling functions to render buttons in array onto screen
+    renderButtons();
+  }
+}
+
+
 function renderButtons() {
-  var citiesAlreadySearched =
-    JSON.parse(localStorage.getItem("citiesAlreadySearched")) || [];
+  //citiesAlreadySearched is global variable
+  //clears out buttons before creating new buttons with new array// prevents duplicates
   btnAppend.innerHTML = "";
 
   for (let index = 0; index < citiesAlreadySearched.length; index++) {
     const createBtnEl = document.createElement("button");
     createBtnEl.classList.add("mb-1");
     createBtnEl.textContent = citiesAlreadySearched[index];
+    //creating event listener for these buttons so i can click and search that same city again
     createBtnEl.addEventListener("click", () => {
       searchCity(citiesAlreadySearched[index]);
     });
@@ -218,9 +234,3 @@ function renderButtons() {
 
 renderButtons();
 
-
-//make serach buttons work
-//add images
-//change date format
-//media a responsive website
-// fix form search
